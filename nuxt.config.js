279e767deeb4e4c2bd0d357e2e99b8333bc86ff6
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-01-06 11:04:59 +0900

    modify url

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,7 +73,7 @@ module.exports = {
   generate: {
     interval: 1000,
     routes(){
-      axios.get(`${apiUrl}/posts?per_page=100&page=1`)
+      axios.get(`${apiUrl}posts?per_page=100&page=1`)
         .then(res => {
           return res.data.map(post => `/posts/${post.id}`)
         })
